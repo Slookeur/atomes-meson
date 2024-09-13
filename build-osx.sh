39e70fc570eada9b48e0dada36569f64f5a12210
Sébastien Le Roux <sebastien.leroux@ipcms.unistra.fr>
2024-09-13 16:11:45 +0200

    Atomes meson: ven. 13 sept. 2024 16:11:45 CEST

--- build-osx.sh
+++ build-osx.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -d /tmp/atomes.app ]; then
+  rm -rf /tmp/atomes.app
+fi
+
 meson setup buildir --prefix=/tmp/atomes.app --bindir=Contents/MacOS -Dbuildtype=release --wipe
 meson compile -C buildir
 meson install -C buildir
--- misc/macos/install_lib.sh
+++ misc/macos/install_lib.sh
@@ -1,27 +1,59 @@
 #!/bin/sh
 
+lib_dir=${MESON_INSTALL_PREFIX}/Contents/Resources/lib
+
 function inst_tool
 {
   lib_path=$1
-  libf=`echo $lib_path|sed 's/\// /g'|awk '{printf $NF}'`
-#  install_name_tool -change "@rapth/$1" "@executable_path/../$libf" ${MESON_INSTALL_PREFIX}/Contents/MacOS/atomes-bin
+  libf=`echo $1|sed 's/\//\n/g'|grep '*.dylib'`
+  install_name_tool -change "@rapth/$1" "@executable_path/../$libf" ${MESON_INSTALL_PREFIX}/Contents/MacOS/atomes-bin
 }
 
-lib_dir=${MESON_INSTALL_PREFIX}/Contents/Resources/lib
-liste_lib=`otool -L atomes-bin|grep '/opt'|awk '{printf $1" "}'`
+function lib_copy_dependency
+{
+  local lib_path=$1
+  local lib=`echo $1|sed 's/\// /g'|awk '{printf $NF}'`
+  echo "Library: "$lib", in : "$lib_path
+  if [ ! -f $lib_dir/$lib ]; then
+    cp -R -L $lib_path $lib_dir
+    local liste_dolib=`otool -L $lib_dir/$lib|grep 'dylib'|grep 'opt'|awk '{printf $1" "}'`    
+    local liste_dopath=`otool -L $lib_path|grep 'rpath'|grep 'dylib'|sed 's/@rpath//g'|awk '{printf $1" "}'`
+    local rpath=`otool -L $lib_path|head -n 1|sed 's/'$lib'//1'|sed 's/\/://g'`
+    #if [ $lib == "libjxl.0.10.dylib" ]; then
+    #  echo "liste_dolib  = "$liste_dolib
+    #  echo "liste_dopath = "$liste_dopath
+    #  echo "rpath        = "$rpath 
+    #fi
+    for dolib in $liste_dolib
+    do 
+      lib_copy_dependency $dolib
+    done
+    # liste_dolib=`otool -L $lib_dir/$lib|grep '/usr'|awk '{printf $1" "}'`
+    # for dolib in $liste_dolib
+    # do 
+    #   lib_copy_dependency $dolib
+    # done
+    for dopath in $liste_dopath
+    do
+      lib_copy_dependency $rpath$dopath
+    done
+  fi
+}
+
+liste_lib=`otool -L atomes-bin|grep 'dylib'|grep 'opt'|awk '{printf $1" "}'`
 mkdir -p $lib_dir
-for lib in $liste_lib
+for llib in $liste_lib
 do
-  echo "Copying : "$lib
-  cp -R -L $lib $lib_dir
-  inst_tool $lib  
+  lib_copy_dependency $llib
+  inst_tool $llib  
 done
 
-#Additionals GCC required lib
-cp -R -L /opt/homebrew/Cellar/gcc/14.1.0_1/lib/gcc/current/libquadmath.0.dylib $lib_dir
-inst_tool libquadmath.0.dylib
-cp -R -L /opt/homebrew/Cellar/gcc/14.1.0_1/lib/gcc/current/libgcc_s.1.1.dylib $lib_dir
-inst_tool libgcc_s.1.1.dylib
+liste_lib=`otool -L atomes-bin|grep 'dylib'|grep 'rpath'|awk '{printf $1" "}'`
+for llib in $liste_lib
+do
+  lib_copy_dependency $llib
+  inst_tool $llib  
+done
 
 #Extra stuff because you'll never know
 cp -R -L /opt/homebrew/lib/gtk-4.0 $lib_dir
